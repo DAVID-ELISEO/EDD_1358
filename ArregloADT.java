a0c7e51363324750843e09c8d461746b114f1b09
DAVID-ELISEO <91902613+DAVID-ELISEO@users.noreply.github.com>
2022-08-30 09:46:58 -0500

    tarea 2

---
+++ ArregloADT.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arregloadt;
+
+/**
+ *
+ * @author hector
+ */
+public class ArregloADT {
+    
+    private int tamanio;
+    private int tam;
+    private T elementos;
+    int longitud;
+    private Arraylist<T> datos;
+    
+    
+    public ArregloADT (int tamanio){
+        this.tamanio=tam;
+        datos=new ArraList();
+        for (int i=0; i<tam; i++){
+         datos.add ((T) new object());  
+            
+        }
+        
+        public void setElemento (T elemento, int indice){
+            
+            if(indice>=0 && indice<this.tamanio){
+                datos.set (indice,elementos);
+                
+            }
+        }
+        public T getElemento (int indice);{
+       if (comprobacion (indice)){
+           return this.dat,get (indice);
+           
+       }
+    private boolean comprobacion (int indice){
+        return indice>=0 && indice <this.tamanio>;
+       
+        
+    }
+       
+       if comprobacion(indice){
+           return this.datos.get (indice):
+       }else{
+    
+    system.out println (for del arreglo);
+}
+       private void setElementos (int indce,dato){
+           if comprobacion (indice){
+               
+           }else{
+               
+           }
+           public int get longitud (){
+               return. this,tamanio;
+               
+           }
+    
+}
+       
+}
+        
+        
+    }
+    
